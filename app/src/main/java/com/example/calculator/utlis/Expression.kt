0ae73f1b299884099ac310004c7f450643b3591f
Stagnant09 <0andy0199@gmail.com>
2025-10-30 15:23:33 +0200

    Graph + Function Graph Screen (WIP)

--- app/src/main/java/com/example/calculator/utlis/Expression.kt
+++ app/src/main/java/com/example/calculator/utlis/Expression.kt
@@ -2,6 +2,44 @@ package com.example.calculator.utlis
 
 import com.example.calculator.models.OperationType
 
+enum class ExpressionForm {
+    CARTESIAN,
+    POLAR
+}
+
+/** e.g. 2x^2 + 3y + 1 = 0
+ * 0 <= x <= 1
+ */
 class Expression {
+    var form: ExpressionForm = ExpressionForm.CARTESIAN
     var terms: List<Term> = emptyList()
+    var limitations: List<String> = emptyList()
+
+    constructor(
+        form: ExpressionForm,
+        terms: List<Term>,
+        limitations: List<String>
+    ){
+        this.form = form
+        this.terms = terms
+        this.limitations = limitations
+    }
+
+    constructor(
+        input: String
+    ){
+        val (form, terms, limitations) = ExpressionParser.parse(input)
+        this.form = form
+        this.terms = terms
+        this.limitations = limitations
+    }
+
+    /** Reorders the terms in the expression in a standard form
+     * Cartesian -> y = f(x)
+     * Polar -> r = f(θ)
+     */
+    fun normalise() {
+        
+    }
+
 }
--- app/src/main/java/com/example/calculator/utlis/ExpressionParser.kt
+++ app/src/main/java/com/example/calculator/utlis/ExpressionParser.kt
@@ -1,2 +1,15 @@
 package com.example.calculator.utlis
 
+object ExpressionParser {
+    fun parse(input: String): Triple<ExpressionForm, List<Term>, List<String>> {
+        return Triple(ExpressionForm.CARTESIAN, emptyList(), emptyList())
+    }
+
+    /** Returns the value of the dependent variable for the given independent variables of
+     * a given expression.
+     * e.g. for the expression 2x^2 + 3y + 1 = 0, if x = 1, then the dependent variable is y = -1
+     */
+    fun evaluate(expression: Expression, independentVariables: Map<Symbol, Double>): Double {
+        return 0.0
+    }
+}
